822154624dd4e150a00c4e6a072143b5c1b444c7
Talemwa Eria Jackson <120446528+Talemwa-Eria-Jackson@users.noreply.github.com>
2024-03-11 13:55:10 +0300

    enhanced the UI of students page

--- resources/views/livewire/student/students.blade.php
+++ resources/views/livewire/student/students.blade.php
@@ -7,11 +7,223 @@
             </ol>
         </div>
         <!-- row -->
-        <div class="row">
-            <div class="col-xl-12 col-lg-12">
+        <div class="row gap-0">
+            <!-- start of the table structure  -->
+            <div class="col-lg-8">
+                <div class="">
+						<div class="card students-list">
+							<div class="card-header border-0 grid grid-cols-12 mt-3 mb-1">
+                                <div>
+                                    <h4 class="text-success">Export CSV</h4>
+                                </div>
+								<div class="">
+									<a class="bg-success text-white py-3 px-3 rounded fw-bold">
+                                        Add Student
+                                    </a>
+								</div>
+							</div>
+							<div class="card-body py-0">
+								<div class="table-responsive">
+									<table class="table display mb-4 dataTablesCard order-table card-table text-black application" id="application-tbl1">
+										<thead>
+											<tr>
+												<th>Name</th>
+												<th>Student ID</th>
+												<th>Email address</th>
+												<th class="text-center">Class</th>
+												<th class="text-center">Gender</th>
+											</tr>
+										</thead>
+										<tbody>
+											<tr>
+												<td>
+													<div class="d-flex align-items-center">
+ 														<img src="images/courses/pic3.jpg" alt="" class="rounded-circle h-10 w-10">
+														<h4 class="mb-0 fs-16 font-w500">Karen Hope</h4>	
+													</div>
+												</td>
+												<td>1234567890</td>
+												<td>hope@email.com</td>
+												<td class="text-center">S5</td>
+												<td class="text-center"><span>Female</span></td>
+											</tr>
+											<tr>
+												<td>
+													<div class="d-flex align-items-center">
+ 														<img src="images/courses/pic3.jpg" alt="" class="rounded-circle h-10 w-10">
+														<h4 class="mb-0 fs-16 font-w500">Karen Hope</h4>	
+													</div>
+												</td>
+												<td>1234567890</td>
+												<td>hope@email.com</td>
+												<td class="text-center">S5</td>
+												<td class="text-center"><span>Female</span></td>
+											</tr>
+											<tr>
+												<td>
+													<div class="d-flex align-items-center">
+ 														<img src="images/courses/pic3.jpg" alt="" class="rounded-circle h-10 w-10">
+														<h4 class="mb-0 fs-16 font-w500">Karen Hope</h4>	
+													</div>
+												</td>
+												<td>1234567890</td>
+												<td>hope@email.com</td>
+												<td class="text-center">S5</td>
+												<td class="text-center"><span>Female</span></td>
+											</tr>
+											<tr>
+												<td>
+													<div class="d-flex align-items-center">
+ 														<img src="images/courses/pic3.jpg" alt="" class="rounded-circle h-10 w-10">
+														<h4 class="mb-0 fs-16 font-w500">Karen Hope</h4>	
+													</div>
+												</td>
+												<td>1234567890</td>
+												<td>hope@email.com</td>
+												<td class="text-center">S5</td>
+												<td class="text-center"><span>Female</span></td>
+											</tr>
+											<tr>
+												<td>
+													<div class="d-flex align-items-center">
+ 														<img src="images/courses/pic3.jpg" alt="" class="rounded-circle h-10 w-10">
+														<h4 class="mb-0 fs-16 font-w500">Karen Hope</h4>	
+													</div>
+												</td>
+												<td>1234567890</td>
+												<td>hope@email.com</td>
+												<td class="text-center">S5</td>
+												<td class="text-center"><span>Female</span></td>
+											</tr>
+											<tr>
+												<td>
+													<div class="d-flex align-items-center">
+ 														<img src="images/courses/pic3.jpg" alt="" class="rounded-circle h-10 w-10">
+														<h4 class="mb-0 fs-16 font-w500">Karen Hope</h4>	
+													</div>
+												</td>
+												<td>1234567890</td>
+												<td>hope@email.com</td>
+												<td class="text-center">S5</td>
+												<td class="text-center"><span>Female</span></td>
+											</tr>
+											<tr>
+												<td>
+													<div class="d-flex align-items-center">
+ 														<img src="images/courses/pic3.jpg" alt="" class="rounded-circle">
+														<h4 class="mb-0 fs-16 font-w500">Karen Hope</h4>
+														
+													</div>
+												</td>
+												<td>1234567890</td>
+												<td>hope@email.com</td>
+												<td class="text-center">S5</td>
+												<td class="text-center"><span>Female</span></td>
+											</tr>
+											<tr>
+												<td>
+													<div class="d-flex align-items-center">
+ 														<img src="images/courses/pic3.jpg" alt="" class="rounded-circle h-8" >
+														<h4 class="mb-0 fs-16 font-w500">Karen Hope</h4>
+														
+													</div>
+												</td>
+												<td>1234567890</td>
+												<td>hope@email.com</td>
+												<td class="text-center">S5</td>
+												<td class="text-center"><span>Female</span></td>
+											</tr>
+										</tbody>
+									</table>
+								</div>
+							</div>
+						</div>
+				</div>
+            </div>
+            <!-- end of the table structure  -->
+
+            <!-- start of the student profile alongside the table  -->
+            <div class="col-xl-4 col-lg-4">
+    <div>
+        <div class="card px-4">
+            <!-- Icons Container -->
+            <div class="d-flex justify-content-between align-items-center mt-3">
+                <!-- Printer Icon -->
+				<div class="fw-bold text-dark">
+				<a href="http://">
+                <i class="bi bi-printer"></i>
+				</a>
+</div>
+                <!-- Delete and Edit Icons -->
+                <div class="d-flex align-items-center">
+				<a href="http://">
+                    <i class="bi bi-trash me-2 text-danger"></i>
+					</a>
+					<a href="http://">
+                    <i class="bi bi-pencil text-success"></i>
+					</a>
+                </div>
+            </div>
+            <div class="mt-5"><h4 class="text-center">1234567898</h4></div>
+            <div class="text-center">
+                <img src="images/egucation-girl.png" alt="std image" class="rounded-circle">
+            </div>
+            <h4 class="text-center pt-2">Karen Hope</h4>
+            <div class="text-center fs-4">S.3</div>
+
+			<div class="d-flex justify-content-center gap-3 py-3">
+				<!-- Graduation Cape Icon with Background Color -->
+				<div >
+					<a href="http://">
+					<i class="bi bi-mortarboard" style="font-size: 1.5rem; background-color: #f8f9fa; padding: 5px; border-radius: 20%;"></i>
+					</a>
+				</div>
+				<!-- Phone Icon with Background Color -->
+				<div >
+				<a href="http://">
+					<i class="bi bi-telephone" style="font-size: 1.5rem; background-color: #f8f9fa; padding: 5px; border-radius: 20%;"></i>
+					</a>
+				</div>
+				<!-- Message Icon with Background Color -->
+				<div >
+				<a href="http://">
+					<i class="bi bi-chat-dots" style="font-size: 1.5rem; background-color: #f8f9fa; padding: 5px; border-radius: 20%;"></i>
+					</a>
+				</div>
+</div>
+
+			
+            <div class="">
+                <h6>About</h6>
+				<div class="mb-4">
+					Karen hope is a smart hardworking girl who is currently in S3. She also doubles as the head prefect. In addtion.........
+				</div>
+            </div>
+            <div class="">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h6>Age</h6>
+                    </div>
+                    <div class="col-md-6">
+                        <h6>Gender</h6>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        17
+                    </div>
+                    <div class="col-md-6">
+                        Female
+                    </div>
+                </div>
+            </div>
+            <div class="fw-bold py-3">People From the same class</div>
+        </div>               
+    </div>
+</div>
+
 
             </div>
         </div>
     </div>
-
 </x-app-layout>
